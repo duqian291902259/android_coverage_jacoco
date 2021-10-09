37031172dd069e1db6f9898a054c430024970195
Dusan <duqian2010@gmail.com>
2021-10-09 17:42:56 +0800

    【Jacoco-Web】重命名，修改pom

--- src/test/java/site/duqian/spring/SpringApplicationTests.java
+++ src/test/java/site/duqian/spring/SpringApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringbootjspApplicationTests {
+class SpringApplicationTests {
 
     @Test
     void contextLoads() {
